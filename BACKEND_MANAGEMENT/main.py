226083edcc09a013ca6006230a2c9bb15f04d978
Ritesh <riteshkumarop420@gmail.com>
2026-08-07 22:45:42 +0530

    change in cors allow origin url

--- BACKEND_MANAGEMENT/main.py
+++ BACKEND_MANAGEMENT/main.py
@@ -8,7 +8,7 @@ app = FastAPI(title="Employee Leave Management API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://visionary-zuccutto-60c274.netlify.app"],
+    allow_origins=["https://employee-leave-management-system-077.netlify.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
